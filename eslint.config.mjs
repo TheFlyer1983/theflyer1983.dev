197eae9edd640e668ee724bc2bf45ec2c9be9d6f
theflyer1983 <theflyer1983@gmail.com>
2026-01-30 16:58:02 +0000

    chore: enhance ESLint configuration with custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
-// Your custom configs here
+export default withNuxt({
+  // Your custom configs here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always'
+        }
+      }
+    ],
+    'vue/block-order': [
+      'error',
+      {
+        order: ['script', 'template', 'style']
+      }
+    ]
+  }
+});

a3c8809cb93b0b7f47facc99ebf09bc88a17efd9
Paul Livingstone <36920884+TheFlyer1983@users.noreply.github.com>
2026-01-30 19:14:02 +0000

    feat: #1 Initial Setup (#3)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/ui',
-    '@nuxt/test-utils'
-  ]
-})
+  modules: ['@nuxt/eslint', '@nuxt/hints', '@nuxt/ui', '@nuxt/test-utils'],
+
+  css: ['./app/assets/css/main.css'],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});

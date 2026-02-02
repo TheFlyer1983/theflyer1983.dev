a47031db7e6621a621c0ddc4913f4e5f1990ff86
theflyer1983 <theflyer1983@gmail.com>
2026-02-02 16:58:28 +0000

    feat: Add CDN Cache-Control headers for Netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,8 @@ export default defineNuxtConfig({
     routeRules: {
       '/**': {
         headers: {
-          'Cache-Control': `public, max-age=${604800}, s-maxage=${604800}`
+          'Cache-Control': `public, max-age=${604800}, s-maxage=${604800}`,
+          'Netlify-CDN-Cache-Control': `public, max-age=${604800}, s-maxage=${604800}`
         }
       }
     }

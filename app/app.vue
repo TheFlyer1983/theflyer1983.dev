fde6984cc43b7dc4a09fefd8c9efc1d98ce6bf90
theflyer1983 <theflyer1983@gmail.com>
2026-02-03 11:50:51 +0000

    feat: Create home landing page with SEO metadata and updated layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+if (import.meta.server) {
+  useSeoMeta({
+    title: 'TheFlyer1983 - Senior Frontend Developer',
+    description:
+      'The personal website of theFlyer1983, a senior frontend developer specializing in Vue.js and Nuxt.js.',
+    ogTitle: 'TheFlyer1983 - Senior Frontend Developer',
+    ogDescription:
+      'The personal website of TheFlyer1983, a senior frontend developer specializing in Vue.js and Nuxt.js.',
+    ogSiteName: 'theflyer1983.dev'
+  });
+}
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div class="flex flex-col h-dvh items-center justify-center">
+      <NuxtLink to="/">Welcome to theflyer1983.dev</NuxtLink>
+      <span>The personal website of Paul Livingstone (also known as <NuxtLink to="https://github.com/theflyer1983" external class="underline">TheFlyer1983</NuxtLink>)</span>
+    </div>
+  </UApp>
 </template>

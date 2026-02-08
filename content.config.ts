8963637cf2cd59bed8a459af648a2b55ceaddde4
theflyer1983 <theflyer1983@gmail.com>
2026-02-08 15:31:46 +0000

    feat: Integrate @nuxtjs/seo module and update content configuration
    - Added @nuxtjs/seo module to enhance SEO capabilities.
    - Refactored content collection to utilize asSeoCollection for better SEO management.
    - Updated content/index.md to include SEO metadata and categories.
    - Removed obsolete robots.txt file as SEO settings are now handled through the content configuration.

--- content.config.ts
+++ content.config.ts
@@ -1,49 +1,32 @@
 import { defineContentConfig, defineCollection } from '@nuxt/content';
+import { asSeoCollection } from '@nuxtjs/seo/content';
 import { z } from 'zod';
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
-      type: 'page',
-      source: '**/*.md',
-      schema: z.object({
-        robots: z
-          .enum([
-            'index, follow',
-            'noindex, follow',
-            'index, nofollow',
-            'noindex, nofollow'
-          ])
-          .default('index, follow'),
-        sitemapPriority: z
-          .enum([
-            '0.0',
-            '0.1',
-            '0.2',
-            '0.3',
-            '0.4',
-            '0.5',
-            '0.6',
-            '0.7',
-            '0.8',
-            '0.9',
-            '1.0'
-          ])
-          .default('0.8'),
-        sitemapChangeFrequency: z
-          .enum([
-            'always',
-            'hourly',
-            'daily',
-            'weekly',
-            'monthly',
-            'yearly',
-            'never'
-          ])
-          .default('weekly'),
-        cannonical: z.string().max(255).url().optional(),
-        sitemapCategories: z.enum(['about', 'contact']).optional()
-      })
-    })
+    content: defineCollection(
+      asSeoCollection(
+        {
+          type: 'page',
+          source: '**/*.md',
+          schema: z.object({
+            robots: z
+              .enum([
+                'index, follow',
+                'noindex, follow',
+                'index, nofollow',
+                'noindex, nofollow'
+              ])
+              .default('index, follow'),
+            categories: z.enum(['about', 'contact']).optional(),
+            /** Fallback for lastmod if sitemap.lastmod is not set. */
+            date: z.string().optional()
+          })
+        },
+        {
+          name: 'content'
+        }
+      )
+    )
   }
 });

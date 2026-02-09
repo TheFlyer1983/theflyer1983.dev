0a0ca08ccf6a0b013f9a0b85c9c7211ee6f4e4c4
theflyer1983 <theflyer1983@gmail.com>
2026-02-09 09:55:39 +0000

    ESlint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,8 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 export default withNuxt({
   // Your custom configs here
   rules: {
-    'no-console': import.meta.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': import.meta.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/html-self-closing': [
       'error',
       {

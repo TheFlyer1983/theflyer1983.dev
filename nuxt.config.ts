c4c0102ab360086710a0a54784a6b776b30f0520
theflyer1983 <theflyer1983@gmail.com>
2026-02-09 10:27:13 +0000

    fix: fixing runtime error with unhead package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,7 +53,7 @@ export default defineNuxtConfig({
 
   nitro: {
     externals: {
-      inline: ['unhead', '@unhead/']
+      inline: ['unhead', '@unhead/*']
     },
     routeRules: {
       '/**': {
